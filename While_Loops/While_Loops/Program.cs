8570eeca34cee5d3e1fd05645067921f0fbb2299
Ben Graham <grahamben7@gmail.com>
2023-06-30 00:45:50 +0200

    While Loops

---
+++ While_Loops/While_Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While Loops!");
+
+            int index = 1;
+
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++; // Incrementing 
+            }
+            
+            int indexDo = 1;
+
+           do
+            {
+                Console.WriteLine(index);
+                indexDo++; // Incrementing 
+            } while (indexDo <= 5) ;
+        }
+    }
+}
