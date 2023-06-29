0c86a39e4cb11b48333e59fbcd5708c6d19dc8e6
Ben Graham <grahamben7@gmail.com>
2023-06-30 00:39:09 +0200

    Goose

---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(0))
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    static string GetDay(int dayNum)
+    {
+        string dayName;
+
+        switch (dayNum)
+        {
+            case 0:
+                dayName = "Sunday";
+                break;
+
+            case 1:
+                dayName = "Monday"; 
+                break;
+
+            case 2:
+                dayName = "Tuesday";
+                break;
+
+            case 3:
+                dayName = "Wednesday";
+                break;
+
+            default:
+                dayName = "Not Valid";
+        }
+
+        return dayName;
+    }
+}
