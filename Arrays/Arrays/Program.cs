e0ba3b32a41be7806665199588a2fa4a9e7f0b3b
Ben Graham <grahamben7@gmail.com>
2023-06-29 22:25:47 +0200

    FInished Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        // Making an array
+        int[] luckyNumbers = { 7, 33, 69, 420, 69420 };
+        
+        string[] friends = new string[10];
+        friends[0] = "Kelly";
+        friends[1] = "Jimmy";
+        
+        Console.WriteLine(luckyNumbers[3]);
+        Console.WriteLine("^_^");
+
+    }
+}
