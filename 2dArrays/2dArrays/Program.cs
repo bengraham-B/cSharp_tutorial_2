4518954f583d0774bfeb0f10176920ccbe6f49cd
Ben Graham <grahamben7@gmail.com>
2023-06-30 13:02:01 +0200

    2D array

--- 2dArrays/2dArrays/Program.cs
+++ 2dArrays/2dArrays/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creating 2D array
+            int[,] numberGrid =
+            {
+                {1, 2},
+                {3, 4},
+                {5, 6 }
+            };
+
+            // How to delcare an array if you do not know what will go in.
+            int[,] myArray = new int[2, 3]; // The 2 is for rows and the 3 for colums
+
+            Console.WriteLine(numberGrid[1, 1]);
         }
     }
 }
