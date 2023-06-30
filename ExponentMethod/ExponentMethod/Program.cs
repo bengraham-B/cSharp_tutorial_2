76a7525ca6dad16d72cec1c5585e699029d2f654
Ben Graham <grahamben7@gmail.com>
2023-06-30 12:57:25 +0200

    2D array

---
+++ ExponentMethod/ExponentMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExponentMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine( GetPow(5, 12));
+        }
+
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+            for(int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+            return result;
+        }
+    }
+}
---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] luckNumbers = { 1, 34, 22, 564, 2028 };
+
+            for (int i = 0; i < luckNumbers.Length; i++)
+            {
+                  Console.WriteLine(luckNumbers[i]);
+
+            }
+        }
+    }
+}
